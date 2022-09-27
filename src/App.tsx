8ad614835a2091236e2a02d13e53d575eb7b2a83
Uzini <43294422+Uziniii@users.noreply.github.com>
2022-09-27 13:31:08 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import Room from './Components/Room'
 function App() {
   const [inviteCode, setInviteCode] = useState("")
 
+  function roomEvent(event: "create" | "join", game: "morpion" | "4pow") {
+    
+  }
+
   return (
-    <div className="w-full h-full flex place-items-center justify-center">
-      { inviteCode === "" && <Room />}
+    <div className="flex justify-center w-full h-full place-items-center">
+      { inviteCode === "" && <Room roomEvent={roomEvent} />}
     </div>
   )
 }
--- src/Components/Room.tsx
+++ src/Components/Room.tsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
 
-export default function Room () {
-    let [game, setGame] = useState("")
+interface Props {
+	roomEvent: (event: "create" | "join", game: "morpion" | "4pow") => void;
+}
 
-    function selectGame(game: string) {
-        setGame(game)
-    }
+export default function Room ({ roomEvent }: Props) {
+	let [game, setGame] = useState<"morpion" | "4pow" | "">("")
 
-    return (
-        <div className="room">
-            <div className="join-room">
-                <input className="w-full border-white border-2" type="text"/>
-            </div>
-            <p className="m-4 text-xl">OU</p>
-            <div className="create-room">
-                <div className="border-white border-2">
-                    <button className={game === "morpion" ? "border-2" : ""} onClick={() => selectGame("morpion")}>Morpion</button>
-                    <button className={game === "4pow" ? "border-2" : ""} onClick={() => selectGame("4pow")}>Puissance 4</button>
-                </div>
-                <button className="text-center text-black border-4 border-white bg-white w-full h-16 mt-2">Create Room</button>
-            </div>
-        </div>
-    )
+	function onClickRoom(event: "create" | "join") {
+		if (event === "create" && game !== "") {
+			
+		}
+	}
+
+	return (
+		<div className="room">
+			<div className="join-room">
+				<input placeholder="Code d'invitation" className="h-16 w-full text-xl text-center border-2 border-white" maxLength={20} type="text"/>
+				<button onClick={() => onClickRoom("join")}>Rejoindre une salle</button>
+			</div>
+			<p className="m-4 text-xl">OU</p>
+			<div className="create-room">
+				<div className="flex justify-around border-2 border-white">
+					<button onClick={() => setGame("morpion")} className={game === "morpion" ? "border-2" : ""}>Morpion</button>
+					<button onClick={() => setGame("4pow")} className={game === "4pow" ? "border-2" : ""}>Puissance 4</button>
+				</div>
+				<button onClick={() => onClickRoom("create")}>Créer une salle</button>
+			</div>
+		</div>
+	)
 }
